441906b1e300fa9a127c5669436a4d03db3088f7
Olga <ove2501@mail.ru>
2022-02-09 22:40:22 +0500

    add py6

---
+++ py6.py
@@ -0,0 +1,16 @@
+#Дано число обозначающее день недели. Вывести его название и указать является ли он выходным
+print('input number day of week')
+a = int(input())
+if (a == 6):
+    print('saturday is weekend')
+elif (a ==7):
+    print('sunday is weekend')
+else:
+    print('sorry')
+
+
+
+
+
+
+
