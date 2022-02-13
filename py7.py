1e6ef081352806c4203695256b6944ddfd87a7f1
Olga <ove2501@mail.ru>
2022-02-13 15:31:11 +0500

    add py7 with bool1

--- py7.py
+++ py7.py
@@ -13,3 +13,6 @@ print((not(x[i-1] or y[i-1] or z[i])) == (not x[i-1] and not y[i-1] and not z[i]
 print((not(x[i-1] or y[i-1] or z[i-1])) == (not x[i-1] and not y[i-1] and not z[i-1]))
 print((not(x[i] or y[i-1] or z[i-1])) == (not x[i] and  not y[i-1] and not z[i-1]))
 print((not(x[i] or y[i] or z[i-1])) == (not x[i] and not y[i] and not z[i-1]))
+print((not(x[i] or y[i-1] or z[i])) == (not x[i] and not y[i-1] and not z[i]))
+print((not(x[i] or y[i-1] or z[i-1])) == (not x[i] and not y[i-1] and not z[i-1]))
+print((not(x[-1] or y[i] or z[i-1])) == (not x[i-1] and not y[i] and not z[i-1]))
