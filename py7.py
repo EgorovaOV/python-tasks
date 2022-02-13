ad6d93b2ac07df827b6eac63a59adaa379dc286b
Olga <ove2501@mail.ru>
2022-02-13 14:30:28 +0500

    add py7 with bool

--- py7.py
+++ py7.py
@@ -1,5 +1,15 @@
 #Проверить истинность утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат
-a = int(input())
-b = int(input())
-c = int(input())
-print((-(a or b or c)) == (-a and -b and -c))
+#a = int(input())
+#b = int(input())
+#c = int(input())
+#print((-(a or b or c)) == (-a and -b and -c))
+
+x, y, z = [True, False], [True, False], [True, False]
+i = 0
+
+print((not(x[i] or y[i] or z[i])) == (not x[i] and not y[i] and not z[i])) 
+print((not(x[i-1] or y[i] or z[i])) == (not x[i-1] and not y[i] and not z[i]))
+print((not(x[i-1] or y[i-1] or z[i])) == (not x[i-1] and not y[i-1] and not z[i]))
+print((not(x[i-1] or y[i-1] or z[i-1])) == (not x[i-1] and not y[i-1] and not z[i-1]))
+print((not(x[i] or y[i-1] or z[i-1])) == (not x[i] and  not y[i-1] and not z[i-1]))
+print((not(x[i] or y[i] or z[i-1])) == (not x[i] and not y[i] and not z[i-1]))
