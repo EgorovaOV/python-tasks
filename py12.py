bb361b794e56b5a90a15d5b11b4e0bd1658edcb1
Olga <ove2501@mail.ru>
2022-02-16 23:01:31 +0500

    add py12 ok

--- py12.py
+++ py12.py
@@ -1,18 +1,16 @@
 #Для натурального n создать словарь индекс-значение, состоящий из элементов последовательности 3n + 1.
 #Для n = 6: {1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6: 19}
-from audioop import add
-
 
 print('enter n',end = " ")
 n = int(input())
 dictionary = {}
 dictionary = \
-   #{
-   #(n - n + 1)  : (3 * n +1)
-    #}
+   {}
 for i in range(n):
-    i = (n-n+1) 
-    dictionary[(i) : (3*i+1)]
+    i = (n-n+1+i) 
+    dictionary[i] = (3*i+1)
+    i+=1
+    
 
 
 print(dictionary)    
