a5cb0b427594a9869415ebfa27017eddc76f7466
Olga <ove2501@mail.ru>
2022-03-07 10:14:29 +0500

    add py30_1

--- py30.py
+++ py30.py
@@ -1,10 +1,15 @@
 #Вычислить число пи  c заданной точностью d
  #   Пример: при d = 0.001,  = 3.141. 10-1d10-10
 import math
-n = 3
-print(' pi: {:.3f}'.format(math.pi))
+n = 6
+print(' pi: {:.30f}'.format(math.pi))
 a = math.pi
-print(round(a,3))
+b = round(a,n+1)*10**n
+c = math.modf(b)
+a = c[1] / 10**n
+print(a)
+
+
 
 
 
