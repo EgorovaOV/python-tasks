1fe868a149b75692337e7815b5f77b34ff1a9b52
Olga <ove2501@mail.ru>
2022-03-17 20:36:27 +0500

    add py35_1

--- py35.py
+++ py35.py
@@ -4,11 +4,20 @@ file = 'file_35.txt'
 f = open(file, 'r')
 data = f.read()
 f.close()
+print(data)
+i = 0
 num = []
-while data != ' ':
-    position = data.index(' ')
-    num.append(int(data[:position]))
-    data = data[position+1:]
+while i < data.index('\n'):
+    num.append(int(data[i]))
+    i += 2
 print(num)
+j = 0
+while j <= (len(num)-1):
+    if num[j+1] - num[j] != 1:
+        print('we find', num[j] + 1)
+    j += 1
+
+
+
 
 
