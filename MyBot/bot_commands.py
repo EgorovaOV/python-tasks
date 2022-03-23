c0550795c953dedacd4403c2ba98b943a1d312d0
Olga <ove2501@mail.ru>
2022-03-23 20:45:10 +0500

    add bot_1

--- MyBot/bot_commands.py
+++ MyBot/bot_commands.py
@@ -1,16 +1,27 @@
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
 import datetime
+from spy import *
 
 
 def hello(update: Update, context: CallbackContext) :
+    log(update, context)
     update.message.reply_text(f'HI, darling {update.effective_user.first_name}!')
 
 def help_command(update: Update, context: CallbackContext) :
-    update.message.reply_text(f'/hello\n/time\n/help\n')
+    log(update, context)
+    update.message.reply_text(f'/hello\n/time\n/help\n/sum\n')
+
 def time_command(update: Update, context: CallbackContext) :
-    update.message.reply_text(f'{datetime.datetime.now().time()}')
-#def sum_command(update: Update, context: CallbackContext) :
- #   update.message.reply_text(f'')
+    log(update, context)
+    update.message.reply_text(f'{datetime.datetime.now()}')#datetime.datetime.now().time()
+def sum_command(update: Update, context: CallbackContext) :
+    log(update, context)
+    msg = update.message.text
+    item = msg.split()# разбить на элементы(сум 1число 2 число)
+    x = int(item[1])
+    y = int(item[2])
+    print(msg)
+    update.message.reply_text(f'{x} + {y} = {x + y}')
 #def mult_command(update: Update, context: CallbackContext) :
  #   update.message.reply_text(f'')
--- MyBot/main.py
+++ MyBot/main.py
@@ -8,7 +8,7 @@ updater = Updater('5183511361:AAGvLxXNPDkxjM5oevdz-Epc9aUpHpOrXTA')
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
 updater.dispatcher.add_handler(CommandHandler('time', time_command))
 updater.dispatcher.add_handler(CommandHandler('help', help_command))
-#updater.dispatcher.add_handler(CommandHandler('sum', sum_command))
+updater.dispatcher.add_handler(CommandHandler('sum', sum_command))
 
 print('server is')
 updater.start_polling()
